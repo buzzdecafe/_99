1b84394b73925a65531675c67539ab746206ecd6
Michael Hurley <mh@buzzdecafe.com>
2017-05-06 14:46:29 -0400

    fix 24

--- ocaml/21-30/24.ml
+++ ocaml/21-30/24.ml
@@ -3,14 +3,24 @@
 The selected numbers shall be returned in a list.
 *)
 
+let rec range m n = 
+  if m = n then [n]
+  else m::(range (m+1) n)
 
+let choose1 xs = 
+  let _ = Random.init(43799) in
+  List.nth xs (Random.int (List.length xs))
 
-let rec lotto_assert n r = 
-  let s = Set.Make(int)
+let rec rm el = function
+  | [] -> []
+  | x::xs -> if x = el then xs else x::(rm el xs) 
 
-  match n with
-  | 0 -> []
-  | _ -> 
+let rec select acc xs = function
+  | 0 -> acc
+  | n -> let x = choose1 xs in
+          select (x::acc) (rm x xs) (n-1)
 
-let _ = assert ((lotto_select 6 49) = [10; 20; 44; 22; 41; 2])
+let lotto_select n r = select [] (range 1 r) n  
+
+let _ = assert ((lotto_select 6 49) = [47; 27; 39; 15; 35; 5])
 
