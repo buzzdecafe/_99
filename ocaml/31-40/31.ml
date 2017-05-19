034f6e8c0d04d258832d924ccb830e2c1f16b5c2
buzzdecafe <m_hur@yahoo.com>
2017-05-19 17:40:51 -0400

    duh

--- ocaml/31-40/31.ml
+++ ocaml/31-40/31.ml
@@ -7,6 +7,6 @@ let rec gcd a b = if b = 0 then a else gcd b (a mod b)
 
 let coprime a b = (gcd a b) = 1
 
-let _ = assert ((coprime 13 27) = true)
-let _ = assert ((not (coprime 20536 7826)) = true)
+let _ = assert (coprime 13 27)
+let _ = assert (not (coprime 20536 7826))
 
